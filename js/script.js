3a914d593a065e3480ca83f12736cae46c04b766
Zariy7 <nazariy.hereha@gmail.com>
2023-11-17 12:50:04 +0100

    push esercizio 1, 2, 3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// 1
+let number = 100;
+console.log(number);
+
+// 2
+const PI = 3.14;
+console.log(PI);
+
+// 2/B - non succede niente, PI è una costante che non viene sovrascritta;
+PI = 3.1416;
